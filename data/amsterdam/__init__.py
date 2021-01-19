8df70f434adfd2fc96a83c3d19cb07beb36507c0
Evgeny Saveliev <e.s.saveliev@gmail.com>
2021-01-19 22:08:43 +0000

    Timegan

---
+++ data/amsterdam/__init__.py
@@ -0,0 +1,12 @@
+"""Hide-and-Seek Privacy Challenge Codebase.
+
+Reference: James Jordon, Daniel Jarrett, Jinsung Yoon, Ari Ercole, Cheng Zhang, Danielle Belgrave, Mihaela van der Schaar, 
+"Hide-and-Seek Privacy Challenge: Synthetic Data Generation vs. Patient Re-identification with Clinical Time-series Data," 
+Neural Information Processing Systems (NeurIPS) Competition, 2020.
+
+Link: https://www.vanderschaar-lab.com/announcing-the-neurips-2020-hide-and-seek-privacy-challenge/
+
+Last updated Date: Jan 19th 2021
+Code author: Jinsung Yoon, Evgeny Saveliev
+Contact: jsyoon0823@gmail.com, e.s.saveliev@gmail.com
+"""
